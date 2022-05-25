7cba7da08e3e5f6a8ece2580bd3dfafbdae5c15f
DinhNgocThai <56432733+Sharrfire@users.noreply.github.com>
2022-05-25 09:34:42 +0700

    Bài 62: Cấu hình sử dụng CSS/SASS

---
+++ src/components/GlobalStyles/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './GlobalStyles.scss';
+
+GlobalStyles.propTypes = {};
+
+function GlobalStyles({ children }) {
+  return children;
+}
+
+export default GlobalStyles;
