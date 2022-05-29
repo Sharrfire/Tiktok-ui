95c304041741a08cfe0ac3ec1842829102fa1aa6
DinhNgocThai <56432733+Sharrfire@users.noreply.github.com>
2022-05-29 16:00:24 +0700

    Bài 69: Xây dựng UI phần Header #5-part2

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -27,12 +27,14 @@ const MENU_ITEMS = [
       title: 'language',
       data: [
         {
-          code: 'en',
-          title: 'English',
-        },
-        {
+          type: 'language',
           code: 'vi',
           title: 'Tiếng việt',
+        },
+        {
+          type: 'language',
+          code: 'en',
+          title: 'English',
         },
       ],
     },
@@ -47,6 +49,15 @@ function Header() {
       setSearchResult([]);
     }, 1000);
   });
+  // handle logic
+  const handleMenuChange = (menuItem) => {
+    switch (menuItem.type) {
+      case ' language':
+        //Handle change language
+        break;
+      default:
+    }
+  };
 
   return (
     <header className={cx('warper')}>
@@ -84,7 +95,7 @@ function Header() {
         <div className={cx('actions ')}>
           <Button text>Upload</Button>
           <Button primary>Login</Button>
-          <Menu items={MENU_ITEMS}>
+          <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
             <button className={cx('more-btn')}>
               <FontAwesomeIcon icon={faEllipsisVertical} />
             </button>
--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -8,26 +8,44 @@ import Header from './Header';
 import { useState } from 'react';
 
 const cx = classNames.bind(styles);
-
-function Menu({ children, items = [] }) {
+const defaultfn = () => {};
+function Menu({ children, items = [], onChange = defaultfn }) {
   const [history, setHistory] = useState([{ data: items }]);
   const current = history[history.length - 1];
   const renderItems = () => {
     return current.data.map((item, index) => {
       const isParent = !!item.children;
-      return <MenuItem key={index} data={item} />;
+
+      return (
+        <MenuItem
+          key={index}
+          data={item}
+          onClick={() => {
+            if (isParent) setHistory((prev) => [...prev, item.children]);
+            else {
+              onChange(item);
+            }
+          }}
+        />
+      );
     });
   };
   return (
     <Tippy
       interactive
       delay={[, 700]}
-      visible
       placement='bottom-end'
       render={(attrs) => (
         <div className={cx('menu-list')} tabIndex='-1' {...attrs}>
           <PopperWrapper className={cx('menu-popper')}>
-            <Header title={cx('Language')} />
+            {history.length > 1 && (
+              <Header
+                title={cx('Language')}
+                onBack={() => {
+                  setHistory((prev) => prev.slice(0, prev.length - 1));
+                }}
+              />
+            )}
 
             {renderItems()}
           </PopperWrapper>
